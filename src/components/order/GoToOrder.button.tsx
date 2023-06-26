de66eba01af0460438319e9c275f391ca1660542
iv <vasis1986@gmail.com>
2023-06-27 01:31:03 +0300

    in progress

--- src/components/order/GoToOrder.button.tsx
+++ src/components/order/GoToOrder.button.tsx
@@ -9,33 +9,29 @@ type Props = {
 const GoToOrderButton = ({ products }: PropsWithChildren<Props>) => {
 
     const { enabled: tgEnabeld = false, tgApi } = useTelegram()
-    const [totalAmount, setTotalAmount] = useState<string | null>(null)
-
-    const onGoToOrder = useCallback(() => {
-        //todo переходим на экран с оплатой
-    }, [])
+    const [totalAmount, setTotalAmount] = useState<number>(0)
 
     useEffect(() => {
-        if (products.length) {
+        if (products && products.length > 0) {
             let total = 0;
             for (let product of products) {
                 total += product.count * product.volume.price
             }
-            setTotalAmount(`Оплатить ${total} ₽`)
+            setTotalAmount(total)
         } else {
-            setTotalAmount(null)
+            setTotalAmount(0)
         }
     }, [products])
 
     useEffect(() => {
-        if (!tgEnabeld) return
-
+        if (!tgApi) return 
+        
         if (totalAmount) {
             tgApi.setMainButtonParams({
                 is_visible: true,
-                text: totalAmount
+                text: 'К оплате' + totalAmount + ' ₽'
             }, () => {
-                onGoToOrder()
+                //todo переходим на экран с оплатой
             })
         } else {
             tgApi.setMainButtonParams({
@@ -43,35 +39,43 @@ const GoToOrderButton = ({ products }: PropsWithChildren<Props>) => {
                 text: ''
             })
         }
-    }, [tgEnabeld, tgApi, totalAmount, onGoToOrder])
+    }, [tgApi, totalAmount])
 
-    if (!tgEnabeld) {
+    if (tgEnabeld) {
         return <>
             <table className="top-bar" style={{ border: 0, width: '100%' }}>
                 <tr>
                     <th>Name</th>
                     <th>Volume</th>
                     <th>Count</th>
-                    <th style={{textAlign: 'right'}}>Amount</th>
+                    <th style={{ textAlign: 'right' }}>Amount</th>
                 </tr>
 
-                {products.map(s => <tr key={s.id + s.volume.volume}>
+                {products && products.map(s => <tr key={s.id + s.volume.volume}>
                     <td className="text-center">{s.name}</td>
                     <td className="text-center">{s.volume.volume}</td>
                     <td className="text-center">{s.count}</td>
-                    <td style={{textAlign: 'right'}} className="text-center">{s.volume.price * s.count}  ₽</td>
+                    <td style={{ textAlign: 'right' }} className="text-center">{s.volume.price * s.count}  ₽</td>
                 </tr>)}
                 <tr>
-                    <td style={{textAlign: 'right'}} colSpan={4} >{totalAmount}</td>
+                    <td style={{ textAlign: 'right' }} colSpan={4} >{totalAmount}</td>
                 </tr>
             </table>
-            
+
         </>
     }
 
     return (
         <>
-            <button onClick={onGoToOrder} type='button' className='button-add-to-cart'>{totalAmount}</button>
+            <button
+                onClick={() => {
+                    //todo переходим на экран с оплатой
+                }}
+                type='button'
+                className='button-add-to-cart'
+            >
+                {totalAmount}
+            </button>
         </>
     )
 }
