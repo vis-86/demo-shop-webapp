46f6b366f0c664eea145ff6771f991e3afd8c976
iv <vasis1986@gmail.com>
2023-11-15 10:49:29 +0300

    in progress

--- src/contexts/cart/TgCartProvider.tsx
+++ src/contexts/cart/TgCartProvider.tsx
@@ -3,76 +3,107 @@ import { ProductInCart } from "@/fetcher/interfaces";
 import { useTelegram } from "@/hooks/useTelegramWebApp";
 import { ReactNode, useCallback, useEffect, useState } from "react";
 import { CartContext } from "./CartContext";
+import { useStartWebApp } from "@/fetcher/user/client";
+
+const enableInitWebApp = false
 
 export const TgCartProvider = ({ children }: { children?: ReactNode | undefined }): ReactNode => {
-    const { tgApp } = useTelegram()
+  const { tgApp } = useTelegram()
 
-    const [tg, setTg] = useState<WebApp | null>(null)
-    const [tgEnabled, setTgEnabled] = useState<boolean>(false)
-    const [tgScriptReady, setTgScriptReady] = useState(false)
-    const [products, setProducts] = useState<ProductInCart[]>([])
+  const [tg, setTg] = useState<WebApp | null>(null)
+  const [tgEnabled, setTgEnabled] = useState<boolean>(false)
+  const [tgScriptReady, setTgScriptReady] = useState(false)
+  const [products, setProducts] = useState<ProductInCart[]>([])
 
-    useEffect(() => {
-        if (tgScriptReady) {
-            const tg = tgApp()
-            setTg(tg)
-            setTgEnabled(tg && tg.initDataUnsafe && tg.initDataUnsafe.query_id ? true : false)
+  const { mutate } = useStartWebApp()
+  const [initWebApp, setInitWebApp] = useState(false)
 
-            //Первоначальные настройки
-            tg?.expand()
-            tg?.enableClosingConfirmation()
+  useEffect(() => {
+    if (!enableInitWebApp) return 
+    if (!tgScriptReady) return
+
+    if (!initWebApp) {
+      try {
+        mutate({
+          userId: tg?.initDataUnsafe.user?.id,
+          username: tg?.initDataUnsafe.user?.username,
+          chatId: tg?.initDataUnsafe.chat?.id
         }
-    }, [tgApp, tgScriptReady, tgEnabled])
-
-    const cartIsEmpty = () => {
-        return products.length === 0
+        ).then(
+          console.log
+        )
+      } catch (e) {
+        console.error(e)
+      } finally {
+        setInitWebApp(true)
+      }
     }
-    const addProduct = useCallback((product: ProductInCart) => {
-        setProducts(state => [...state, product])
-    }, [])
+  }, [tg, tgScriptReady, initWebApp, mutate])
 
-    const updateProduct = (product: ProductInCart) => {
-        setProducts(products => products.map(
-            item => item.uniqId === product.uniqId
-                ? { ...item, ...product }
-                : item
-        ))
+  useEffect(() => {
+    if (tgScriptReady) {
+      const tg = tgApp()
+      setTg(tg)
+      setTgEnabled(tg && tg.initDataUnsafe && tg.initDataUnsafe.query_id ? true : false)
+
+      //Первоначальные настройки
+      tg?.expand()
+      tg?.enableClosingConfirmation()
+      // tg?.requestContact((result) => {
+      //   console.log('get contact:', result)
+      // })
     }
+  }, [tgApp, tgScriptReady, tgEnabled, mutate])
 
-    const removeProduct = (product: ProductInCart) => {
-        setProducts(products => products.filter(item => item.uniqId !== product.uniqId))
+  const cartIsEmpty = () => {
+    return products.length === 0
+  }
+  const addProduct = useCallback((product: ProductInCart) => {
+    setProducts(state => [...state, product])
+  }, [])
+
+  const updateProduct = (product: ProductInCart) => {
+    setProducts(products => products.map(
+      item => item.uniqId === product.uniqId
+        ? { ...item, ...product }
+        : item
+    ))
+  }
+
+  const removeProduct = (product: ProductInCart) => {
+    setProducts(products => products.filter(item => item.uniqId !== product.uniqId))
+  }
+
+  const totalAmount = (): number => {
+    let total = 0
+    for (const item of products) {
+      if (!item || !item.volume) {
+        continue
+      }
+      total += item.count * item.volume.price
     }
+    return total
+  }
 
-    const totalAmount = (): number => {
-        let total = 0
-        for (const item of products) {
-            if (!item || !item.volume) {
-                continue
-            }
-            total += item.count * item.volume.price
-        }
-        return total
-    }
-
-    return (
-        <CartContext.Provider value={{
-            products,
-            tgEnabled,
-            tg,
-            totalAmount,
-            cartIsEmpty: cartIsEmpty,
-            addProduct: addProduct,
-            updateProduct: updateProduct,
-            removeProduct: removeProduct,
-        }}>
-            <Script
-                src="https://telegram.org/js/telegram-web-app.js"
-                onLoad={() => {
-                    setTgScriptReady(true)
-                }} />
-            {children}
-        </CartContext.Provider>
-    )
+  return (
+    <CartContext.Provider value={{
+      products,
+      tgEnabled,
+      tg,
+      totalAmount,
+      cartIsEmpty: cartIsEmpty,
+      addProduct: addProduct,
+      updateProduct: updateProduct,
+      removeProduct: removeProduct,
+    }}>
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        onLoad={() => {
+          setTgScriptReady(true)
+        }} />
+      {children}
+    </CartContext.Provider>
+  )
 }
 
 export default CartContext
